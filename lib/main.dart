c6ab6e712ce5fb4bf79888ebb63cebc240d6a053
xSILENCEx <894659061@qq.com>
2023-12-13 21:04:03 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,219 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:fluent_ui/fluent_ui.dart' hide Page;
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:system_theme/system_theme.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'theme.dart';
+
+const String appTitle = 'Hoyo Launcher';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemTheme.accentColor.load();
+  await flutter_acrylic.Window.initialize();
+  await flutter_acrylic.Window.hideWindowControls();
+  await WindowManager.instance.ensureInitialized();
+
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitleBarStyle(
+      TitleBarStyle.hidden,
+      windowButtonVisibility: false,
+    );
+    await windowManager.setMinimumSize(const Size(500, 600));
+    await windowManager.show();
+    await windowManager.setPreventClose(true);
+    await windowManager.setSkipTaskbar(false);
+  });
+
+  runApp(const MyApp());
+}
+
+final _appTheme = AppTheme();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = _appTheme;
+    return FluentApp(
+      title: appTitle,
+      themeMode: appTheme.mode,
+      debugShowCheckedModeBanner: false,
+      color: appTheme.color,
+      darkTheme: FluentThemeData(
+        brightness: Brightness.dark,
+        accentColor: appTheme.color,
+        visualDensity: VisualDensity.standard,
+        focusTheme: FocusThemeData(
+          glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+        ),
+      ),
+      theme: FluentThemeData(
+        accentColor: appTheme.color,
+        visualDensity: VisualDensity.standard,
+        focusTheme: FocusThemeData(
+          glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+        ),
+      ),
+      locale: appTheme.locale,
+      builder: (context, child) {
+        return Directionality(
+          textDirection: appTheme.textDirection,
+          child: NavigationPaneTheme(
+            data: NavigationPaneThemeData(
+              backgroundColor:
+                  appTheme.windowEffect != flutter_acrylic.WindowEffect.disabled ? Colors.transparent : null,
+            ),
+            child: child!,
+          ),
+        );
+      },
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  int _selected = 0;
+
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = AppTheme();
+
+    return NavigationView(
+      appBar: const NavigationAppBar(
+        automaticallyImplyLeading: false,
+        title: DragToMoveArea(
+          child: Align(
+            alignment: AlignmentDirectional.centerStart,
+            child: Text(appTitle),
+          ),
+        ),
+        actions: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            WindowButtons(),
+          ],
+        ),
+      ),
+      pane: NavigationPane(
+        selected: _selected,
+        size: const NavigationPaneSize(openMaxWidth: 200, compactWidth: 50),
+        displayMode: appTheme.displayMode,
+        items: <NavigationPaneItem>[
+          PaneItem(
+            icon: const Icon(FluentIcons.home, size: 50),
+            title: const Text('Home'),
+            body: Container(color: Colors.black),
+            onTap: () => setState(() => _selected = 0),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.home),
+            title: const Text('Home'),
+            body: Container(color: Colors.blue),
+            onTap: () => setState(() => _selected = 1),
+          ),
+        ],
+        footerItems: <NavigationPaneItem>[
+          PaneItem(
+            icon: const Icon(FluentIcons.settings),
+            title: const Text('Settings'),
+            body: Container(color: Colors.green),
+            onTap: () async {
+              // launchUrlString(r'D:\StarRail\Star Rail\Game\StarRail.exe');
+              final String? dirPath = await FilePicker.platform.getDirectoryPath();
+              print('dirPath:$dirPath');
+
+              if (dirPath == null) return;
+
+              launchUrlString(dirPath);
+            },
+          ),
+        ],
+        indicator: () {
+          switch (appTheme.indicator) {
+            case NavigationIndicators.end:
+              return const EndNavigationIndicator();
+            case NavigationIndicators.sticky:
+            default:
+              return const StickyNavigationIndicator();
+          }
+        }(),
+        autoSuggestBoxReplacement: const Icon(FluentIcons.search),
+      ),
+    );
+  }
+
+  @override
+  void onWindowClose() async {
+    bool isPreventClose = await windowManager.isPreventClose();
+    if (isPreventClose && mounted) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return ContentDialog(
+            title: const Text('Confirm close'),
+            content: const Text('Are you sure you want to close this window?'),
+            actions: [
+              FilledButton(
+                child: const Text('Yes'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  windowManager.destroy();
+                },
+              ),
+              Button(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FluentThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
+
+final rootNavigatorKey = GlobalKey<NavigatorState>();
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import macos_window_utils
+import package_info_plus
+import path_provider_foundation
+import screen_retriever
+import sqflite
+import sqlite3_flutter_libs
+import system_theme
+import url_launcher_macos
+import window_manager
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+}
